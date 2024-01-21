34d23f4743839753f6040cd7aab4a4953f6c8d2f
oxangyal <Oxana.mitch@gmail.com>
2024-01-21 15:57:37 -0500

    Updated register with more errors handling

--- controllers/sessionController.js
+++ controllers/sessionController.js
@@ -6,24 +6,57 @@ const registerShow = (req, res) => {
 };
 
 const registerDo = async (req, res, next) => {
-    if (req.body.password != req.body.password1) {
+  let validation_errors = false
+  if (req.body.password != req.body.password1) {
     req.flash("error", "The passwords entered do not match.");
-    res.render("register", {errors: req.flash.errors});
-    }
-    try {
+    validation_errors = true
+  }
+
+  try {
     await User.create(req.body);
-    } catch (e) {
+  } catch (e) {
     if (e.constructor.name === "ValidationError") {
-        parseVErr(e, req);
+      parse_v(e, req);
     } else if (e.name === "MongoServerError" && e.code === 11000) {
-        req.flash("error", "That email address is already registered.");
+      req.flash("error", "That email address is already registered.");
     } else {
-        return next(e);
+      return next(e);
     }
-    return res.render("register");
-    }
+    validation_errors = true
+  }
+  
+  // if there are no validation errors then redirect to main paige 
+  // else pass errors to be rendered in the register view
+
+  if (! validation_errors)
+  {
     res.redirect("/");
+  }
+  else
+  {
+    return res.render("register", {errors: req.flash("error")})
+  }
+
 };
+// const registerDo = async (req, res, next) => {
+//     if (req.body.password != req.body.password1) {
+//         req.flash("error", "The passwords entered do not match.");
+//         res.render("register", {errors: req.flash.errors});
+//     }
+//     try {
+//         await User.create(req.body);
+//     } catch (e) {
+//     if (e.constructor.name === "ValidationError") {
+//         parseVErr(e, req);
+//     } else if (e.name === "MongoServerError" && e.code === 11000) {
+//         req.flash("error", "That email address is already registered.");
+//     } else {
+//         return next(e);
+//     }
+//     return res.render("register");
+//     }
+//     res.redirect("/");
+// };
 
 const logoff = (req, res) => {
     req.session.destroy(function (err) {
