5fb0e64614c7f519749509b40f23b0e7e4efb0de
oxangyal <Oxana.mitch@gmail.com>
2024-01-21 00:09:28 -0500

    Config passport, protecting routes, implemented CSRF

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
-const express = require("express");
-require("express-async-errors");
-require("dotenv").config();
-const session = require("express-session");
-const MongoDBStore = require("connect-mongodb-session")(session);
+const express = require('express');
+require('express-async-errors');
+require('dotenv').config();
+
+const session = require('express-session');
 
 const app = express();
 
-app.set("view engine", "ejs");
-app.use(require("body-parser").urlencoded({ extended: true }));
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const store = new MongoDBStore({
-    // may throw an error, which won't be caught
-    uri: process.env.MONGO_URL,
-    collection: "mySessions",
+  // may throw an error, which won't be caught
+    uri: process.env.MONGO_URI,
+    collection: 'mySessions',
 });
-store.on("error", function (error) {
+store.on('error', function (error) {
     console.log(error);
 });
 
@@ -23,76 +22,76 @@ const sessionParms = {
     resave: true,
     saveUninitialized: true,
     store: store,
-    cookie: { secure: false, sameSite: "strict" },
+    cookie: { secure: false, sameSite: 'strict' },
 };
 
-if (app.get("env") === "production") {
-    app.set("trust proxy", 1); // trust first proxy
-    sessionParms.cookie.secure = true; // serve secure cookies
+if (app.get('env') === 'production') {
+  app.set('trust proxy', 1); // trust first proxy
+  sessionParms.cookie.secure = true; // serve secure cookies
 }
 
 app.use(session(sessionParms));
+const passport = require('passport');
+const passportInit = require('./passport/passportInit');
 
-app.use(require("connect-flash")());
+passportInit();
+app.use(passport.initialize());
+app.use(passport.session());
 
-// app.use(
-//   session({
-//     secret: process.env.SESSION_SECRET,
-//     resave: false,
-//     saveUninitialized: true,
-//   })
-// );
+const cookieParser = require('cookie-parser');
+app.use(cookieParser(process.env.SESSION_SECRET));
+
+app.set('view engine', 'ejs');
+app.use(require('body-parser').urlencoded({ extended: true }));
+
+const csrf = require('host-csrf');
+let csrf_development_mode = true;
+if (app.get('env') === 'production') {
+    csrf_development_mode = false;
+    app.set('trust proxy', 1);
+}
+const csrf_options = {
+    protected_operations: ['PATCH'],
+    protected_content_types: ['application/json'],
+    development_mode: csrf_development_mode,
+};
+app.use(csrf(csrf_options));
+
+app.use(require('connect-flash')());
+
+app.use(require('./middleware/storeLocals'));
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.use('/sessions', require('./routes/sessionRouter'));
 
 // secret word handling
-let secretWord = "syzygy";
-app.get("/secretWord", (req, res) => {
-    // res.render("secretWord", { secretWord });
-    if (!req.session.secretWord) {
-        req.session.secretWord = "syzygy";
-    }
-    res.locals.info = req.flash("info");
-    res.locals.errors = req.flash("error");
-    res.render("secretWord", { secretWord: req.session.secretWord });
-});
-// app.post("/secretWord", (req, res) => {
-//   secretWord = req.body.secretWord;
-//   res.redirect("/secretWord");
-// });
-app.post("/secretWord", (req, res) => {
-    if (req.body.secretWord.toUpperCase()[0] == "P") {
-        req.flash("error", "That word won't work!");
-        req.flash("error", "You can't use words that start with p.");
-    } else {
-        req.session.secretWord = req.body.secretWord;
-        req.flash("info", "The secret word was changed.");
-    }
-    // res.render("secretWord", {
-    //   secretWord,
-    //   errors: flash("errors"),
-    //   info: flash("info"),
-    // });
-    res.redirect("/secretWord");
-});
+const secretWordRouter = require('./routes/secretWord');
+const auth = require('./middleware/auth');
+
+app.use('/secretWord', auth, secretWordRouter);
 
 app.use((req, res) => {
-    res.status(404).send(`That page (${req.url}) was not found.`);
+  res.status(404).send(`That page (${req.url}) was not found.`);
 });
 
 app.use((err, req, res, next) => {
-    res.status(500).send(err.message);
-    console.log(err);
+  res.status(500).send(err.message);
+  console.log(err);
 });
 
 const port = process.env.PORT || 7000;
 
 const start = async () => {
-    try {
-        app.listen(port, () =>
-            console.log(`Server is listening on port ${port}...`)
-        );
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    await require('./db/connect')(process.env.MONGO_URI);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 start();
